6a2ac92adddc9cdd0bf0c655033aeab2e9a79dda
bahati-hakizimana <hakizimanabahati2019@gmail.com>
2024-04-04 06:06:37 -0700

    Game

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	fmt.Println("Well come to Suitch case Golang Game")
+
+
+	rand.Seed(time.Now().UnixNano())
+	diceNumber := rand.Intn(6) + 1
+
+	fmt.Println("Value of dice is:", diceNumber)
+
+	switch diceNumber {
+	case 1:
+		fmt.Println("Dice value is 1 and you can open")
+	case 2:
+		fmt.Println("You can move 2 spot")
+	case 3:
+		fmt.Println("You can move 3 spot")
+		fallthrough
+	case 4:
+		fmt.Println("You can move 4 spot")
+		fallthrough
+	case 5:
+		fmt.Println("You can move 5 spot")
+	case 6:
+		fmt.Println("You can move 6 spot and role the dice again")
+	
+	default:
+		fmt.Println("What was that ?")
+		
+	}
+}
+
